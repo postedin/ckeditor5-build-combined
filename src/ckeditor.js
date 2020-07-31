1e039a084c41b4faf28fdff1c448ec843eb58ba7
robclancy <robbo.clancy@gmail.com>
2020-07-31 06:52:16 -0400

    include simple upload adapter in exports

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,6 +1,7 @@
 import ClassicEditor from './classic-editor';
 import CommentEditor from './comment-editor';
 import DecoupledEditor from './decoupled-document-editor';
+import SimpleUploadAdapter from './simple-upload-adapter';
 import UpcastWriter from '@ckeditor/ckeditor5-engine/src/view/upcastwriter';
 
-export default { ClassicEditor, CommentEditor, DecoupledEditor, UpcastWriter };
+export default { ClassicEditor, CommentEditor, DecoupledEditor, SimpleUploadAdapter, UpcastWriter };
