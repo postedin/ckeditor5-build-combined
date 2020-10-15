859a02f32a8dfef3b95b9fe7c2c9bec60d030a34
robclancy <robbo.clancy@gmail.com>
2020-10-14 22:20:16 -0300

    feat: updated to ckeditor 23. enabled new list-style plugin on the document editor. disabled our link paste overriding since it is buggy

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -6,28 +6,28 @@ import DecoupledEditor from './decoupled-document-editor';
 import SimpleUploadAdapter from './simple-upload-adapter';
 
 function injectExternalLinkPaste(editor) {
-  const writer = new UpcastWriter(editor.editing.view.document);
-
-  editor.plugins.get('Clipboard').on('inputTransformation', (evt, data) => {
-    const element = data.content.getChild(0);
-
-    if (element.name === 'a') {
-      writer.setAttribute('target', '_blank', element);
-      writer.setAttribute('rel', 'noopener noreferrer', element);
-
-      data.content = writer.createDocumentFragment([
-        writer.createElement(
-          element.name,
-          {
-            href: element.getAttribute('href'),
-            target: element.getAttribute('target'),
-            rel: element.getAttribute('rel'),
-          },
-          [writer.createText(element.getAttribute('href'))],
-        ),
-      ]);
-    }
-  }, { priority: 'high' });
+  // const writer = new UpcastWriter(editor.editing.view.document);
+  //
+  // editor.plugins.get('Clipboard').on('inputTransformation', (evt, data) => {
+  //   const element = data.content.getChild(0);
+  //
+  //   if (element.name === 'a') {
+  //     writer.setAttribute('target', '_blank', element);
+  //     writer.setAttribute('rel', 'noopener noreferrer', element);
+  //
+  //     data.content = writer.createDocumentFragment([
+  //       writer.createElement(
+  //         element.name,
+  //         {
+  //           href: element.getAttribute('href'),
+  //           target: element.getAttribute('target'),
+  //           rel: element.getAttribute('rel'),
+  //         },
+  //         [writer.createText(element.getAttribute('href'))],
+  //       ),
+  //     ]);
+  //   }
+  // }, { priority: 'high' });
 }
 
 export default {
